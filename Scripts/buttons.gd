93bab0b898e58f5bac4e02399de12c2e39ee6f61
CarterBaxter <CarterWBaxter@gmail.com>
2025-08-14 18:17:04 -0600

    Spiffed Up title and Credits

--- Scripts/buttons.gd
+++ Scripts/buttons.gd
@@ -1,26 +1,31 @@
 extends Node
 
+@onready var select: AudioStreamPlayer = %select
+@onready var hover: AudioStreamPlayer = %hover
 
 
-func send_to_local():
+func send_to_local() -> void:
 	get_tree().change_scene_to_file("res://Game Screens/local.tscn")
 	
-func send_to_online():
+func send_to_online() -> void:
 	get_tree().change_scene_to_file("res://Game Screens/online.tscn")
 	
-func send_to_credits():
+func send_to_credits() -> void:
 	get_tree().change_scene_to_file("res://Game Screens/credits.tscn")
 	
-func send_to_settings():
+func send_to_settings() -> void:
 	get_tree().change_scene_to_file("res://Game Screens/settings.tscn")
 	
-func send_to_main_menu():
+func exit_game() -> void:
+	get_tree().quit()
+	
+func send_to_main_menu() -> void:
 	get_tree().change_scene_to_file("res://Game Screens/main_menu.tscn")
 
 
 #sound scripts#
 func on_hover():
-	$AudioStreamPlayer.play()
+	hover.play()
 	
 func on_click():
-	$AudioStreamPlayer2.play()
+	select.play()
