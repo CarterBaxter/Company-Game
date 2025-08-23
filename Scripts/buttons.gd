bb7c0033978362f2672265966be088b5a5c214d6
Lixtopl <brian@krw5.com>
2025-08-23 11:07:21 -0600

    character and character select screen
    added a character and game test scene. I also added a character select scene with nothing really in it rn.

--- Scripts/buttons.gd
+++ Scripts/buttons.gd
@@ -1,22 +1,25 @@
 extends Node
 
 func send_to_local() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Main Scenes/local.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main Scenes/Menu scenes/local.tscn")
 	
 func send_to_online() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Main Scenes/online.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main Scenes/Menu scenes/online.tscn")
 	
 func send_to_credits() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Main Scenes/credits.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main Scenes/Menu scenes/credits.tscn")
 	
 func send_to_settings() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Main Scenes/settings.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main Scenes/Menu scenes/settings.tscn")
+	
+func send_to_gametest_screen() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Main Scenes/game_scenes/gametemp.tscn")
 	
 func exit_game() -> void:
 	get_tree().quit()
 	
 func send_to_main_menu() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Main Scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main Scenes/Menu scenes/main_menu.tscn")
 
 
 #sound scripts#
